c0a687d083ff77bd814c32d6bd9004d93cb05df8
Xavier Fuchs <Xavier Fuchs@FUCHSBOX-W10>
2022-06-05 21:47:57 +0200

    playing around some more

--- My first python anything/My_first_python_anything.py
+++ My first python anything/My_first_python_anything.py
@@ -1,3 +1,21 @@
 test = "Hello World"
-
 print(test)
+
+
+
+atm= "learning python"
+print(atm)
+
+
+a = "I am learning "
+b = "Python"
+
+c = a+b
+
+print(c)
+
+print("I am so good at " + b)
+
+
+Grade = "A"
+print("Xaviers' grade in English: " + Grade)
